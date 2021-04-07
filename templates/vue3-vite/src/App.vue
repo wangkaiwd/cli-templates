2f27cce360b4409019f26d55c7f7346bc6fc8506
王凯03 <wangkai03@weidian.com>
2021-04-07 11:59:36 +0800

    feat: add some template for test

---
+++ templates/vue3-vite/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Vue3 template</div>
+</template>
+
+<script>
+
+export default {
+  setup () {
+    console.log('setup');
+  }
+};
+</script>
+
+<style scoped>
+#app {
+
+}
+</style>
